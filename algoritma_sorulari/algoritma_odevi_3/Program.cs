38b7f42b94c1f1ab091b5b7f186b6c3316e77cfc
“[firstname <anilcan_fb@hotmail.com>
2022-03-12 14:54:06 +0300

    Assignment Algorithm Question 3 added

---
+++ algoritma_sorulari/algoritma_odevi_3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace algoritmaSorusu3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen Bir Sayı Giriniz : ");
+            int n = int.Parse(Console.ReadLine());
+            string[] kelimeDizisi = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Lütfen {0}. Kelimeyi Giriniz.",i+1);
+                kelimeDizisi[i] = Convert.ToString(Console.ReadLine());
+            }
+
+            foreach (var kelime in kelimeDizisi)
+            {
+                Console.WriteLine(kelime);
+            }
+            
+            Array.Reverse(kelimeDizisi);
+            foreach (var kelime in kelimeDizisi)
+            {
+                Console.WriteLine(kelime);
+            }
+        }
+    }
+}
